bbe8e5c3ecaeb5a66b9343cb4f005b912af700b6
Ruhan Islam <ruhanislam@hotmail.com>
2024-04-24 20:36:08 -0400

    Added changes for feature/ruhan

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+    author: "Ruhan",
+    poem: "I drive car \n I go far \n I look up \n I see star",
+    image: "star.jpg",
+  }, 
 ];
 
 let displayPoems = (array) => {
